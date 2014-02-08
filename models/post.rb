b8998eeaaf8476f4639ca2ff8fc01f7c247af6af
tvetan <tvetan@gbg.bg>
2014-02-08 20:13:11 +0200

    Add header and footer to the layout page using bootstrap

--- models/post.rb
+++ models/post.rb
@@ -2,14 +2,20 @@ require 'mongoid'
 
 class Post
   include Mongoid::Document
- 
+  include Mongoid::Versioning
+  include Mongoid::Timestamps
+
   field :title,   type: String
   field :content, type: String
-
   field :permalink, type: String, default: -> { make_permalink }
+
   has_and_belongs_to_many :tags
 
   def make_permalink
+    slug
+  end
+
+  def slug
     title.downcase.gsub(/W/,'-').squeeze('-').chomp('-') if title
   end
 end
--- url_helpers.rb
+++
@@ -1,9 +0,0 @@
-module UrlHelpers
-    def new_todo_path
-      "/todos/new"
-    end
-
-    def link_to(text, path, options = {})
-      %Q{<a href='#{path}' class='#{options[:class]}' data-method='#{options[:method]}'>#{text}</a>'}
-    end
-end
